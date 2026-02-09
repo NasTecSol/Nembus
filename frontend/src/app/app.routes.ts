6034b9fcfac2bb228f902aa63442e6e1528342d6
Shoaib-1910 <shoaib.sardar@nastecsol.com>
2026-02-09 13:51:03 +0500

    structure re arrange fixed

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,27 @@
 import { Routes } from '@angular/router';
-import { SetupComponent } from './components/setup/setup.component';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
+import { SetupComponent } from './features/setup/setup.component';
+import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
+
+// export const routes: Routes = [
+//     { path: 'setup', component: SetupComponent },
+//     { path: 'login', component: LoginComponent },
+//     { path: 'home', component: HomeComponent },
+//     { path: '', redirectTo: '/login', pathMatch: 'full' }
+// ];
+
 
 export const routes: Routes = [
-    { path: 'setup', component: SetupComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+  {
+    path: "",
+    loadChildren: () =>
+      import("./features/auth/auth-routes").then((m) => m.Auth_ROUTES),
+  },
+//   {
+//     path: "",
+//     loadChildren: () =>
+//       import("./features/dashboard-layout/dashboard-layout.routes").then(
+//         (m) => m.DASHBOARD_LAYOUT_ROUTES
+//       ),
+//   },
 ];
